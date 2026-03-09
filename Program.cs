628479ddaa76f23dddb250da744eee5c4e19b32e
Roman Klimovich <s27619@pjwstk.edu.pl>
2026-03-09 16:53:20 +0100

    Add basic handling for user input

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Calculator");
-Console.WriteLine("Enter numbers separated by space:");
+        Console.WriteLine("Calculator");
+        Console.WriteLine("Enter numbers separated by space:");
+
+        string input = Console.ReadLine();
+        string[] parts = input.Split(' ');
+    }
+}
